cd8f1250bed75886b30654b9d1348e7b339b77dc
jht6 <juhaotian@foxmail.com>
2021-08-12 20:40:58 +0800

    增加提示

--- shell/docker-mongo-ubuntu.sh
+++ shell/docker-mongo-ubuntu.sh
@@ -3,9 +3,11 @@
 
 # 拉取镜像
 docker pull mongo:5.0-focal
+echo "[OK] 已拉取镜像"
 
 # 启动mongo服务
 docker run -d -p 27017:27017 -v mongo_configdb:/data/configdb -v mongo_db:/data/db --name mongo mongo:5.0-focal
+echo "[OK] 已启动mongo服务"
 
 echo "可执行以下命令启动mongo shell:"
 echo "docker exec -it mongo mongo admin"
